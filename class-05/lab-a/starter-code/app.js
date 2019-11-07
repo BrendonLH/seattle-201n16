0bb3c4ad4926c129aa4b99e71ec9c174e7c23d1b
Brendon Hampton <brendonhampton1988@gmail.com>
2019-11-06 19:39:54 -0800

    finished Question 2

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,12 +8,20 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-
+//  eslint-disable-line
+function sum(num1, num2){
+  var numbers = num1 + num2;
+  var wordString = 'The sum of ' + num1 + ' and ' + num2 + ' is ' + numbers + '.';
+  var newArray = [numbers,wordString];
+  return newArray;
 }
 
+console.log(sum(4,5)[0]);
+console.log(sum(4,5)[1]);
+console.log(sum(4,5));
+
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -21,17 +29,24 @@ function sum(a, b) { //eslint-disable-line
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
+
 "The product of 5 and 9 is 45."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(num1, num2) { //eslint-disable-line
+  var sum = num1 * num2;
+  var wordString = 'The product of ' + num1 + ' and ' + num2 + ' is ' + sum + '.';
+  var newArray = [sum,wordString];
+  return newArray;
 }
+console.log(multiply(5,9)[0]);
+console.log(multiply(5,9)[1]);
+
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -97,7 +112,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
